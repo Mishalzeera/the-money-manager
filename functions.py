215c6dda2c8d7284b64bf93f0abf867d848652f0
Mishalzeera <78355230+Mishalzeera@users.noreply.github.com>
2021-10-01 15:20:15 +0200

    Migrate to Gitpod until Mongo issue is resolved.

---
+++ functions.py
@@ -0,0 +1,21 @@
+def euros_to_cents(euros_float):
+    # takes in a float of euros, * 100 and returns an int of cents
+    return int(euros_float * 100)
+
+
+def cents_to_euros(cents_int):
+    # takes in an int of cents, / 100 and returns a float of euros
+    return float(cents_int / 100)
+
+
+def new_invoice_income(invoice_amount):
+    # takes in an amount and returns what the user can spend
+    tax = invoice_amount - invoice_amount/1.21
+    total = invoice_amount - tax
+    return total
+
+
+def new_invoice_tax(invoice_amount):
+    # takes in an amount and returns what the user should save for taxes
+    tax = invoice_amount - invoice_amount/1.21
+    return tax
