5e6ccedd68f707c3991a4d2756023980a3a901e8
Mishalzeera <78355230+Mishalzeera@users.noreply.github.com>
2021-10-01 08:28:14 +0200

    Add uri string, database.

--- env.py
+++
@@ -1,7 +0,0 @@
-import os
-
-os.environ.setdefault("IP", "0.0.0.0")
-os.environ.setdefault("PORT", "5500")
-os.environ.setdefault("MONGO_URI", "")
-os.environ.setdefault("SECRET_KEY", "1029384756")
-os.environ.setdefault("MONGO_DB", "")
